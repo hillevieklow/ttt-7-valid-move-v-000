be63bb08cf3d43379249be10e228582f0738cd18
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-23 21:24:03 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   position = index.to_i - 1
   if !position_taken?(board, position) && position.between?(0,8)
     true
-  else
+  if position_taken?(board, position) && position.between?(0,8)
     false
   end
 end
